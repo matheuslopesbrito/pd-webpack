680261fa34e6e0bf48707e3d9b3306839398bdbb
Matheus Lopes <matfsdev@gmail.com>
2022-02-01 13:43:55 -0300

    Padrão webpack formado

--- src/armaments.js
+++
@@ -1,4 +0,0 @@
-export default {
-    laser: ["Phasers", "Torpedos de Fóton"],
-    explosion: ["Canhão", "Torpedos de TNT"]
-}
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Spaceship from "./spaceship"
-import armamentsKind from "./armaments"
-
-const spaceship = new Spaceship("USS Enterprise", "Matheus", armamentsKind.laser)
-
-console.log(spaceship)
